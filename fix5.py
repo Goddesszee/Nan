b961c04980fc05de4d557be9dd86f17a489618eb
Goddesszee <zaradanjumafoundation@gmail.com>
2026-05-22 09:21:19 +0100

    fix: syntax error in doSwap

---
+++ fix5.py
@@ -0,0 +1,9 @@
+﻿c=open('index.html','r',encoding='utf-8').read()
+lines=c.split('\n')
+# Remove lines 2205-2209 (index 2204-2208) which are leftover old code
+print('Before:',len(lines),'lines')
+print('Removing:',lines[2205:2210])
+del lines[2205:2210]
+print('After:',len(lines),'lines')
+open('index.html','w',encoding='utf-8').write('\n'.join(lines))
+print('Done')
---
+++ fix8.py
@@ -0,0 +1,8 @@
+﻿c=open('index.html','r',encoding='utf-8').read()
+lines=c.split('\n')
+print('Removing lines 2230-2237:')
+for i in range(2229,2237):
+    print(i+1,repr(lines[i][:80]))
+del lines[2229:2237]
+open('index.html','w',encoding='utf-8').write('\n'.join(lines))
+print('Done')
