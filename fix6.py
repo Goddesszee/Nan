c2109ec277c3fae2edb2d07e4e64c2db89d689ad
Zara Fatolu <156331119+Goddesszee@users.noreply.github.com>
2026-05-23 11:35:18 +0100

    Cleanup: remove unused file fix6.py

--- fix6.py
+++
@@ -1,7 +0,0 @@
-﻿c=open('index.html','r',encoding='utf-8').read()
-lines=c.split('\n')
-print('Line 2205:',repr(lines[2204][-50:]))
-print('Line 2206:',repr(lines[2205]))
-del lines[2205]
-open('index.html','w',encoding='utf-8').write('\n'.join(lines))
-print('Done')
